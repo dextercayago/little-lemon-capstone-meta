e1b4dc665f6f67b7a4bcc753b99ac8eaf732148f
Dexter <redexecute@gmail.com>
2023-04-24 16:25:02 -0500

    fixed nav font

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
 import { useState } from "react";
+import logo from "../assets/nav-logo.svg"
 
 
 const Navbar = () => {
@@ -10,29 +11,30 @@ const Navbar = () => {
         setNav(!nav)
     }
     return (
-        <div className="flex text-xl mt-5 justify-between items-center h-10 max-w-[1240px] mx-auto px-4">
-            <h1 className="w-full text-3xl font-bold text-[#00df9a] mt-25"> Logo
-            </h1>
+        <div className="flex text-2xl text-primary-gray font-bold mt-5 justify-between items-center h-10 max-w-[1240px] mx-auto px-4">
+            <a href="/"><img src={logo} /></a>
             <ul className="hidden md:flex">
-                <li className="p-4">About</li>
                 <li className="p-4">Home</li>
+                <li className="p-4">About</li>
                 <li className="p-4">Menu</li>
                 <li className="p-4">Reservation</li>
                 <li className="p-4">Order</li>
                 <li className="p-4">Login</li>
             </ul>
-            <div onClick={handleNav} className='block md:hidden'>
+            <div className='block md:hidden'>
+            <div onClick={handleNav} >
             {!nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
             </div>
-            <div className={!nav ? 'fixed text-xl left-0 mt-5 top-12 w-[60%] border-r bourder-r-gray-900 h-full bg-[#edefee] ease-in-out duration-500 mt-150' : 'fixed left-[-100%]' }>
+            <div className={!nav ? 'fixed text-primary-gray text-xl  font-bold left-0 mt-5 top-12 w-[60%] border-r bourder-r-gray-900 h-full bg-[#edefee] ease-in-out duration-500 mt-150' : 'fixed left-[-100%]' }>
             <ul className="pt-4">
-                <li className="p-4 border-b border-gray-600">About</li>
                 <li className="p-4 border-b border-gray-600">Home</li>
+                <li className="p-4 border-b border-gray-600">About</li>
                 <li className="p-4 border-b border-gray-600">Menu</li>
                 <li className="p-4 border-b border-gray-600">Reservation</li>
                 <li className="p-4 border-b border-gray-600">Order</li>
                 <li className="p-4">Login</li>
             </ul>
+            </div>
             </div>
         </div>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{ 
+        'primary-gray':'#495e57',
+        'primary-yellow' : '#f4ce14',
+        'secondary-dark' : '#EE9972',
+        'secondary-light' : '#FBDABB',
+        'highlight-ligh' : '#EDEFEE',
+        'highlight-dark' : '#333333'
+    }
+
+    },
   },
   plugins: [],
 }
