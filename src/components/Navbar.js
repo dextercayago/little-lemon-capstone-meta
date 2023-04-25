bc2ccda20214a1baab55ef7309564ad6ac13f5be
Dexter <redexecute@gmail.com>
2023-04-24 22:18:23 -0500

    nav bar complete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,14 +12,14 @@ const Navbar = () => {
     }
     return (
         <div className="flex text-2xl text-primary-gray font-bold mt-5 justify-between items-center h-10 max-w-[1240px] mx-auto px-4">
-            <a href="/"><img src={logo} /></a>
+            <a href="/"><img src={logo} alt="logo" /></a>
             <ul className="hidden md:flex">
-                <li className="p-4">Home</li>
-                <li className="p-4">About</li>
-                <li className="p-4">Menu</li>
-                <li className="p-4">Reservation</li>
-                <li className="p-4">Order</li>
-                <li className="p-4">Login</li>
+                <li className="p-4 hover:text-secondary-dark">Home</li>
+                <li className="p-4 hover:text-secondary-dark">About</li>
+                <li className="p-4 hover:text-secondary-dark">Menu</li>
+                <li className="p-4 hover:text-secondary-dark">Reservation</li>
+                <li className="p-4 hover:text-secondary-dark">Order</li>
+                <li className="p-4 hover:text-secondary-dark">Login</li>
             </ul>
             <div className='block md:hidden'>
             <div onClick={handleNav} >
@@ -27,12 +27,12 @@ const Navbar = () => {
             </div>
             <div className={!nav ? 'fixed text-primary-gray text-xl  font-bold left-0 mt-5 top-12 w-[60%] border-r bourder-r-gray-900 h-full bg-[#edefee] ease-in-out duration-500 mt-150' : 'fixed left-[-100%]' }>
             <ul className="pt-4">
-                <li className="p-4 border-b border-gray-600">Home</li>
-                <li className="p-4 border-b border-gray-600">About</li>
-                <li className="p-4 border-b border-gray-600">Menu</li>
-                <li className="p-4 border-b border-gray-600">Reservation</li>
-                <li className="p-4 border-b border-gray-600">Order</li>
-                <li className="p-4">Login</li>
+                <li className="p-4 border-b border-gray-600 hover:text-secondary-dark">Home</li>
+                <li className="p-4 border-b border-gray-600 hover:text-secondary-dark">About</li>
+                <li className="p-4 border-b border-gray-600 hover:text-secondary-dark">Menu</li>
+                <li className="p-4 border-b border-gray-600 hover:text-secondary-dark">Reservation</li>
+                <li className="p-4 border-b border-gray-600 hover:text-secondary-dark">Order</li>
+                <li className="p-4 hover:text-secondary-dark">Login</li>
             </ul>
             </div>
             </div>
